716635ead8766ec1c128398ec2af548b54b9f78e
Isheta Divya <ishetadivya@gmail.com>
2019-12-11 10:48:22 +0530

    search from url and auto refresh added

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -1,22 +1,38 @@
-let timeLeft = 3; // todo
+let timeLeft = 30; 
 let counterElement;
 let newsElement;
+let queryParam;
+let searchBar;
 
 let timerId = setInterval(countdown, 1000);
 
+// Initialises the timer with 30 s and keep running the counter from 30s to 0s
+
 function countdown() {
     counterElement = document.getElementById('counter');
-    if (timeLeft === -1) {
-        clearTimeout(timerId);
-        getNews();
+    counterElement.innerHTML = timeLeft;
+    if (timeLeft === 0) {
+        timeLeft = 30;
     } else {
-        counterElement.innerHTML = timeLeft+"";
+        counterElement.innerHTML = timeLeft;
         timeLeft--;
     }
-} //todo
+} 
+
+window.onload = function(){
+    queryParam = window.location.href.split("=")[1];
+    searchBar = this.document.getElementById('newsSearch');
+    searchBar.value = queryParam;
+    this.getNews(queryParam,1);
+}
+
+// It hits the api and get the list of news based on query parameter passed
+
 function getNews(searchText = "", pageNumber = 0) {
     let xmlhttp = new XMLHttpRequest();
-    const url = "https://newsapi.org/v2/everything?q=";
+    // let url = window.location.href;
+    // url += "query="+searchText.value;
+    const api_url = "https://newsapi.org/v2/everything?q=";
     const apiKey = "e989f78cdb494492bca304f1f7579827";
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -30,24 +46,39 @@ function getNews(searchText = "", pageNumber = 0) {
         }
     };
 
-    xmlhttp.open('GET', url+searchText.value+"&apiKey="+apiKey+"&pageSize=10&page="+pageNumber);
+    xmlhttp.open('GET', api_url+searchText+"&apiKey="+apiKey+"&pageSize=50&page="+pageNumber);
     xmlhttp.send();
 }
+
+// It gets the response as parameter and display the news in UI
+
 function displayNews(news){
     newsElement = document.getElementById("news");
     let newsDiv = document.createElement('div');
+    newsDiv.className = "newsDiv";
+    newsDiv.id = "news-list";
     let containerDiv;
     let newsImg;
     var newsTitleDiv = document.createElement('div');
     let newsDescDiv = document.createElement('div');
+    let listElem;
     news.articles.map(data => {
         newsImg = document.createElement("img");
         newsImg.setAttribute("height", "50");
         newsImg.setAttribute("width", "50");
-        containerDiv = document.createElement('div'); // want to use this in 2nd append child
-        // newsElement.appendChild(newsDiv).append("<div> <div><img src='"+data.urlToImage+" '></div> <div>"+data.title+"</div> <div>"+data.content+"</div> </div>");
+        containerDiv = document.createElement('div'); 
         newsElement.appendChild(newsDiv).appendChild(document.createElement('div')).appendChild(newsImg).setAttribute("src",data.urlToImage);
         newsElement.appendChild(newsDiv).appendChild(document.createElement('div')).innerHTML = ""+data.title;
         newsElement.appendChild(newsDiv).appendChild(document.createElement('div')).innerHTML = ""+data.content;
     });
+
+// Detect when news list is scrolled
+
+    listElem = document.getElementById('news-list');
+    listElem.addEventListener('scroll', function() {
+        if (listElem.scrollTop + listElem.clientHeight >= listElem.scrollHeight) {
+            displayNews(news);
+        }
+      });
+    
 }
